4544eec38768ce87f59db8692ce6a99041aa4f29
Nicole Dow <nikki.dow71@gmail.com>
2019-08-05 19:33:00 +0000

    initial commit

---
+++ lib/weather_scraper.rb
@@ -0,0 +1,6 @@
+require "weather_scraper/version"
+
+module WeatherScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/weather_scraper/version.rb
@@ -0,0 +1,3 @@
+module WeatherScraper
+  VERSION = "0.1.0"
+end
