d661cfe8addb0ae825ec5092930c15c8bad788fb
Nicole Dow <nikki.dow71@gmail.com>
2019-08-05 21:15:23 +0000

    begin method for cli

---
+++ lib/CLI.rb
@@ -0,0 +1,16 @@
+class CLI
+
+  def begin
+    puts "Hello! Welcome to the City Info Daily gem."
+    dialogue
+  end
+
+  def dialogue
+   #puts menu and asks to select a city to view
+  end
+
+
+
+
+
+end
