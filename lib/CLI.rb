8bee437d07c2677b84e06d59d25572870707e70d
Nicole Dow <nikki.dow71@gmail.com>
2019-08-05 23:24:14 +0000

    begin and menu methods

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,11 +2,13 @@ class CLI
 
   def begin
     puts "Hello! Welcome to the City Info Daily gem."
-    dialogue
+    menu
   end
 
-  def dialogue
+  def menu
    #puts menu and asks to select a city to view
+   puts "Please enter a United States zipcode to see that city's data."
+   user_input = gets.strip
   end
 
 
